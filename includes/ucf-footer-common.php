230e9f5fd37058d6546487321b116d056230ddc1
Jim Barnes <jimbarnesdeveloper@gmail.com>
2018-02-02 14:55:02 -0500

    Added nofollow to the telephone link.

--- includes/ucf-footer-common.php
+++ includes/ucf-footer-common.php
@@ -62,7 +62,7 @@ if ( !class_exists( 'UCF_Footer_Common' ) ) {
 				<?php echo self::display_social_links(); ?>
 				<?php echo self::display_nav_links(); ?>
 				<p class="ucf-footer-address">
-					4000 Central Florida Blvd. Orlando, Florida, 32816 | <a href="tel:4078232000">407.823.2000</a>
+					4000 Central Florida Blvd. Orlando, Florida, 32816 | <a rel="nofollow" href="tel:4078232000">407.823.2000</a>
 					<br>
 					&copy; <a href="https://www.ucf.edu/">University of Central Florida</a>
 				</p>
